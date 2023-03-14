a938fb32598b41411f0b510c5813214681c6bfb8
Andrey Shorokhov <arshor@rambler.ru>
2023-03-14 20:18:42 +0500

    Done MainPageRetScooter and DropDownListQuestionTest, add constants DropDownList

--- src/main/java/pageObjectRentScooter/MainPageRentScooter.java
+++ src/main/java/pageObjectRentScooter/MainPageRentScooter.java
@@ -1,18 +1,14 @@
 package pageObjectRentScooter;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import static org.junit.Assert.assertEquals;
-
 public class MainPageRentScooter {
 
     // Локаторы
-
     // Кнопка принятия куки "да все привыкли"
     private By cookieButton = By.id("rcc-confirm-button");
     // Кнопка "Заказать" в хедере
@@ -22,26 +18,29 @@ public class MainPageRentScooter {
     // Кнопка "Заказать" в середине страницы
     private By middleOrderButton = By.className("Button_Button__ra12g Button_Middle__1CSJM");
     // Массив вопросов в разделе "Вопросы о важном"
-    private By listQuestion_0 = By.id("accordion__heading-0");
-    private By listQuestion_1 = By.id("accordion__heading-1");
-    private By listQuestion_2 = By.id("accordion__heading-2");
-    private By listQuestion_3 = By.id("accordion__heading-3");
-    private By listQuestion_4 = By.id("accordion__heading-4");
-    private By listQuestion_5 = By.id("accordion__heading-5");
-    private By listQuestion_6 = By.id("accordion__heading-6");
-    private By listQuestion_7 = By.id("accordion__heading-7");
+    private static final By[] DROP_DOWN_LIST_QUESTIONS = new By[]{
+            By.id("accordion__heading-0"),
+            By.id("accordion__heading-1"),
+            By.id("accordion__heading-2"),
+            By.id("accordion__heading-3"),
+            By.id("accordion__heading-4"),
+            By.id("accordion__heading-5"),
+            By.id("accordion__heading-6"),
+            By.id("accordion__heading-7")};
     // Массив ответов в разделе "Вопросы о важном"
-    private By listAnswer_0 = By.xpath(".//div[@id='accordion__panel-0']/p");
-    private By listAnswer_1 = By.xpath(".//div[@id='accordion__panel-1']/p");
-    private By listAnswer_2 = By.xpath(".//div[@id='accordion__panel-2']/p");
-    private By listAnswer_3 = By.xpath(".//div[@id='accordion__panel-3']/p");
-    private By listAnswer_4 = By.xpath(".//div[@id='accordion__panel-4']/p");
-    private By listAnswer_5 = By.xpath(".//div[@id='accordion__panel-5']/p");
-    private By listAnswer_6 = By.xpath(".//div[@id='accordion__panel-6']/p");
-    private By listAnswer_7 = By.xpath(".//div[@id='accordion__panel-7']/p");
+    private static final By[] DROP_DOWN_LIST_ANSWERS = new By[]{
+            By.xpath(".//div[@id='accordion__panel-0']/p"),
+            By.xpath(".//div[@id='accordion__panel-1']/p"),
+            By.xpath(".//div[@id='accordion__panel-2']/p"),
+            By.xpath(".//div[@id='accordion__panel-3']/p"),
+            By.xpath(".//div[@id='accordion__panel-4']/p"),
+            By.xpath(".//div[@id='accordion__panel-5']/p"),
+            By.xpath(".//div[@id='accordion__panel-6']/p"),
+            By.xpath(".//div[@id='accordion__panel-7']/p")};
+
     private WebDriver driver;
 
-    public MainPageRentScooter(WebDriver driver){
+    public MainPageRentScooter(WebDriver driver) {
         this.driver = driver;
     }
 
@@ -57,20 +56,14 @@ public class MainPageRentScooter {
         driver.findElement(middleOrderButton).click();
     }
 
-    public void scrollPageToEnd() {
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", driver.findElement(listQuestion_7));
+    public void clickQuestion(int num_item) {
+        driver.findElement(DROP_DOWN_LIST_QUESTIONS[num_item]).click();
     }
 
-    public void clickQuestion_0() {
-        new WebDriverWait(driver, 5).until(ExpectedConditions.elementToBeClickable(listQuestion_0));
-        driver.findElement(listQuestion_0).click();
-    }
-
-    public void checkAnswer_0() {
-        new WebDriverWait(driver, 5).until(ExpectedConditions.visibilityOfElementLocated(listAnswer_0));
-        String answerText = driver.findElement(listAnswer_0).getText();
-        String expectedText = "Сутки — 400 рублей. Оплата курьеру — наличными или картой.";
-        assertEquals(expectedText, answerText);
+    public String getAnswer(int num_item) {
+        WebElement dropDownItem = new WebDriverWait(driver, 5).until(ExpectedConditions.visibilityOfElementLocated(DROP_DOWN_LIST_ANSWERS[num_item]));
+        String answerText = dropDownItem.getText();
+        return answerText;
     }
 
 }
--- src/test/java/DropDownListQuestionTest.java
+++ src/test/java/DropDownListQuestionTest.java
@@ -1,32 +1,62 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.junit.Test;
 import pageObjectRentScooter.MainPageRentScooter;
 
+import static org.junit.Assert.assertEquals;
+import static pageObjectRentScooter.constants.DropDownList.EXPECTED_ANSWERS;
+import static pageObjectRentScooter.constants.DropDownList.QUESTIONS;
+
+@RunWith(Parameterized.class)
 public class DropDownListQuestionTest {
 
     private WebDriver driver;
+    private final String question;
+    private final String expectedAnswer;
+    private final int num_item;
+    public static final String PAGE_URL = "https://qa-scooter.praktikum-services.ru/";
+
+    public DropDownListQuestionTest(String question, String expectedAnswer, int num_item) {
+        this.question = question;
+        this.expectedAnswer = expectedAnswer;
+        this.num_item = num_item;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getDropDownListData() {
+        return new Object[][] {
+                { QUESTIONS[0], EXPECTED_ANSWERS[0], 0},
+                { QUESTIONS[1], EXPECTED_ANSWERS[1], 1},
+                { QUESTIONS[2], EXPECTED_ANSWERS[2], 2},
+                { QUESTIONS[3], EXPECTED_ANSWERS[3], 3},
+                { QUESTIONS[4], EXPECTED_ANSWERS[4], 4},
+                { QUESTIONS[5], EXPECTED_ANSWERS[5], 5},
+                { QUESTIONS[6], EXPECTED_ANSWERS[6], 6},
+                { QUESTIONS[7], EXPECTED_ANSWERS[7], 7},
+        };
+    }
 
     @Before
     public void setUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        driver.get(PAGE_URL);
     }
 
     @Test
-    public void CheckDropDownListQuestionText() {
-
-        driver.get("https://qa-scooter.praktikum-services.ru/");
+    public void checkDropDownList_ShowCorrectAppearsText() {
 
         MainPageRentScooter objMainPageRentScooter = new MainPageRentScooter(driver);
 
         objMainPageRentScooter.clickCookieButton();
-//        objMainPageRentScooter.scrollPageToEnd();
-        objMainPageRentScooter.clickQuestion_0();
-        objMainPageRentScooter.checkAnswer_0();
+        objMainPageRentScooter.clickQuestion(num_item);
+        String answer = objMainPageRentScooter.getAnswer(num_item);
+        assertEquals("Текст ответа не совпадает в вопросе: " + question, expectedAnswer, answer);
     }
 
     @After
